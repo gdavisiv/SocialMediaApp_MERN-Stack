fc47c93c14556fdd789bd5ff84f421a87e4eac6a
GDIV <gdavisiv@gmail.com>
2022-08-31 20:46:05 -0400

    Backend Server Running

--- server/index.js
+++ server/index.js
@@ -5,7 +5,16 @@ import cors from 'cors';
 
 const app = express();
 
-//Settingup body parses to properly send out request
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://Admin:qLIM34GZyy7fp32uf1jn234@cluster0.yktbcpe.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+//mongoose.set('useFindAndModify', false);
