2efd2d80770240d7f5b5040b5731a3f92e111e9b
GDIV <gdavisiv@gmail.com>
2022-09-09 08:58:12 -0400

    Setting up Post Structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
-import memories from './images/memories.png'
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import memories from './images/memories.png';
+
 const App = () => {
   return (
     <Container maxwidth="lg">
