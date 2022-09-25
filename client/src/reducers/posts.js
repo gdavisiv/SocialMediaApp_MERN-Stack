1157eee4e63dafe095d7186dffe1808b8d462503
GDIV <gdavisiv@gmail.com>
2022-09-24 20:11:21 -0400

    Updated Comments to post file

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,3 +1,5 @@
+//Reducer is a function that is equal to a function that accepts 
+//a STATE and a FUNCTION
 const reducer = (posts = [], action) => {
  switch (action.type) {
     case 'FETCH_ALL':
