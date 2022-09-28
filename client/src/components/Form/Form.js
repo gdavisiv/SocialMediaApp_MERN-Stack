79704c6c367e013afdaa24120467866a1449757e
GDIV <gdavisiv@gmail.com>
2022-09-27 21:30:02 -0400

    Implementing TextFields

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { TextField, Button, Typography, Paper  } from '@material-ui/core';
-
+import FileBase from 'react-file-base64';
 import useStyles from './styles';
 
 const Form = () => {
@@ -16,14 +16,22 @@ const Form = () => {
     <Paper className={classes.paper}>
       <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSumbit}>
         <Typography variant="h6">Creating a Memory</Typography>
-        <TextField 
-          name="creator" 
-          variant="outlined" 
-          label="Creator" 
-          fullWidth
-          value={postData.creator}
-          onChange={(e) => setPostData({ ...postData, creator: e.target.value })} 
+        {/*   
+          //If we add a second text field we would over-ride our original,
+          //to fix this we can simply use '...postData' to make the data persist
+          //and only change the data in the specific text field
+        */}
+        <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
+        <TextField name="creator" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
+        <TextField name="creator" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
+        <TextField name="creator" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })} />
+        <div className={classes.fileInput}>
+          <FileBase
+            type="file"
+            multiple={false}
+            oneDone={({base64}) => setPostData({ ...postData, selectedFile: base64})}
           />
+        </div>
       </form>
     </Paper>
   );
