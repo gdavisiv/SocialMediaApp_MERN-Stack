e38cb4de9872d9d4adbe27ca1c7fedaea7db9405
GDIV <gdavisiv@gmail.com>
2022-09-27 20:59:41 -0400

    Building out Form Submit

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { TextField, Button, Typography, Paper  } from '@material-ui/core';
 
 import useStyles from './styles';
 
@@ -7,7 +8,11 @@ const Form = () => {
   const classes = useStyles();  
 
   return (
-    <h1>Form</h1>
+    <Paper className={classes.paper}>
+      <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSumbit}>
+
+      </form>
+    </Paper>
   );
 };
 
