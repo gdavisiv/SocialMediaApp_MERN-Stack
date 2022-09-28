74c592e3b9567c7978ab657e75da5efff763ec68
GDIV <gdavisiv@gmail.com>
2022-09-27 21:13:45 -0400

    Creating TextField Functionality

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField, Button, Typography, Paper  } from '@material-ui/core';
 
 import useStyles from './styles';
 
 const Form = () => {
+  const [postData, setPostData ] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
 
-  const classes = useStyles();  
+  const classes = useStyles();
+  
+  const handleSumbit = () => {
+
+  };
 
   return (
     <Paper className={classes.paper}>
       <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSumbit}>
-
+        <Typography variant="h6">Creating a Memory</Typography>
+        <TextField 
+          name="creator" 
+          variant="outlined" 
+          label="Creator" 
+          fullWidth
+          value={postData.creator}
+          onChange={(e) => setPostData({ ...postData, creator: e.target.value })} 
+          />
       </form>
     </Paper>
   );
 };
 
-export default Form
+export default Form;
