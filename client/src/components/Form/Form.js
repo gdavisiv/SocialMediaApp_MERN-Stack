12c97ac1ccf9be331e0e5a81b7ebb199bb893b46
GDIV <gdavisiv@gmail.com>
2022-10-31 10:20:43 -0400

    Updated Reducer and Form

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -12,7 +12,7 @@ const Form = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
   
-  const handleSumbit = () => {
+  const handleSumbit = (e) => {
     e.preventDefault();
 
     dispatch(createPost(postData));
