175cd645db45ab6cfc72f35baf66d6e3c2b0decb
GDIV <gdavisiv@gmail.com>
2022-11-01 21:00:09 -0400

    Build Post/Testing odd Error message

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 
 import useStyles from './styles';
+
+import { FormHelperText } from '@material-ui/core';
 
 const Post = () => {
   const classes = useStyles();
